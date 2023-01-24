abb0b7b35f095a33f8f59ed7707a4bac32b42049
Tim von Asen <timvonasen@users.noreply.github.com>
2023-01-24 22:18:29 +0100

    Static Question Number
    Questions will be random

--- agents-of-shield/pages/quiz.jsx
+++ agents-of-shield/pages/quiz.jsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { AppContext } from "../pages/createContext";
 
 /* function randomInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -8,20 +9,34 @@ import { useSupabaseClient } from "@supabase/auth-helpers-react";
 export default function Quiz() {
   const supabase = useSupabaseClient();
   const [questionsDatabase, setQuestionsDatabase] = useState(0);
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  const [questionCounter, setQuestionCounter] = useState(0);
+  /* Make sure questions will never be the same */
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(
+    Math.floor(Math.random() * 3)
+  );
+  const [questionCounter, setQuestionCounter] = useState(
+    1 /* User defined Value */
+  );
   const [score, setScore] = useState(0);
-  const [showScore, setShowScore] = useState(false);
-  const questionCheck = () => {
-    //This will increment the currentQuestionIndex by 1
-    setCurrentQuestionIndex(currentQuestionIndex + 1);
+
+  const { currentAppState, setCurrentAppState } = useContext(AppContext);
+
+  const handleAnwserButtonClick = (correct) => {
+    /* Adds 1 to the score if the answer is correct */
+    if (correct === true) {
+      setScore(score + 1);
+    }
+
+    //This will increment the currentQuestionIndex by a random number
+    setCurrentQuestionIndex(
+      currentQuestionIndex + 1 + Math.floor(Math.random() * 4)
+    );
     //This is to check how many questions have been answered
     setQuestionCounter(questionCounter + 1);
     //Anytime the currentQuestionIndex is equal to the length of the questionsDatabase array, we want to reset the currentQuestionIndex to 0
     if (currentQuestionIndex === questionsDatabase.length - 1) {
       setCurrentQuestionIndex(0);
     }
-    if (questionCounter === questionsDatabase.length) {
+    if (questionCounter === 12 - 1) {
       showScore(true);
     }
   };
@@ -46,26 +61,31 @@ export default function Quiz() {
     <div>
       {/* Score that will be displayed at the end of the quiz */}
       {showScore ? (
-        setCurrentAppState("quiz")
+        setCurrentAppState("score")
       ) : (
         /* If it is not the specified end, questions are asked */
         <body>
           <div>
             {questionsDatabase.length > 0 && (
               <div class="quizContainer">
+                <p>Frage {questionCounter} von 12</p>
                 <h3>{questionsDatabase[currentQuestionIndex].question}</h3>
                 {questionsDatabase[currentQuestionIndex].answers.map(
                   (answerOption) => {
                     return (
                       <div key={answerOption.value}>
-                        <button onClick={() => questionCheck()}>
+                        <button
+                          onClick={() =>
+                            handleAnwserButtonClick(answerOption.correct)
+                          }
+                        >
                           {answerOption.value}
                         </button>
                       </div>
                     );
                   }
                 )}
-                <h3>Score</h3>
+                <p>{score} von 12</p>
               </div>
             )}
           </div>
--- agents-of-shield/pages/start.jsx
+++ agents-of-shield/pages/start.jsx
@@ -4,12 +4,19 @@ import { AppContext } from "../pages/createContext";
 
 export default function Start() {
   const { currentAppState, setCurrentAppState } = useContext(AppContext);
+  const numberOfQuestions = "";
+
   return (
     <div className="startContainer">
       <p>
         Ihr Prüfer: <b>Phillip J. Coulson</b>{" "}
       </p>
       <TextField label="Vollständiger Name" required></TextField>
+      {/*       <TextField
+        label="Anzahl der Fragen"
+        required
+        value={numberOfQuestions}
+      ></TextField> */}
 
       <button onClick={() => setCurrentAppState("quiz")}>Teilnehmen</button>
     </div>
